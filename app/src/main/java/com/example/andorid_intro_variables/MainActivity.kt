5dda821368482da3fa85daad26d7453fbee74fb3
Martin Blum <ozzmab@hotmail.com>
2021-07-28 13:49:47 -0300

    first commit

---
+++ app/src/main/java/com/example/andorid_intro_variables/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.andorid_intro_variables
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+
+}
